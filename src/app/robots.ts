54999bc04e06c36687bfe0d37fcddca344b57a77
Marcos Quintana <kutu94737@gmail.com>
2026-07-19 15:55:23 +0200

    fix(seo): point robots to production domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,9 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/api/',
-        },
-        sitemap: 'https://mkode.com/sitemap.xml',
-    }
+  return {
+    rules: { userAgent: "*", allow: "/", disallow: "/api/" },
+    sitemap: "https://www.mkode.es/sitemap.xml",
+    host: "https://www.mkode.es",
+  };
 }
